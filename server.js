b71c6f9de9ed453bf4c79a3eafdcc5c1520df3e6
Nirnay Jain <nirujainofficial@gmail.com>
2021-03-17 14:23:05 +0530

    added jwt authentication

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const  express=require('express')
 const app=express()
 const mongoose=require('mongoose')
 const route=require('./routes/auth')
+const postRoute=require('./routes/post')
 require('dotenv/config')
 mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopology: true });
   app.use(express.json())
 
 
 app.use('/api/user',route)
+app.use('/api/posts',postRoute)
 
 
 const port=process.env.PORT||3000
--- validation.js
+++ validation.js
@@ -15,3 +15,4 @@ const schema=Joi.object({
 return schema.validate(data)
 }
 module.exports.registerValidation=registerValidation
+module.exports.loginValidation=loginValidation
